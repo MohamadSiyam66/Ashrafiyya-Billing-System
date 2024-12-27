7067e04d9db4594ca8e38ccf858dee86c8ceb6f8
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-12-27 19:07:18 +0530

    chor:update name

--- src/main/java/org/billingsys/ashrafiyabillingsystem/BillingController.java
+++ src/main/java/org/billingsys/ashrafiyabillingsystem/BillingController.java
@@ -3,7 +3,7 @@ package org.billingsys.ashrafiyabillingsystem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class BillingController {
     @FXML
     private Label welcomeText;
 
